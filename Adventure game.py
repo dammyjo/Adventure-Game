5bec6b7c7d73df4a0a116f5f85d626ce8aec9e89
dammyjo <112008172+dammyjo@users.noreply.github.com>
2022-10-07 18:18:53 +0100

    Add files via upload

---
+++ Adventure game.py
@@ -0,0 +1,38 @@
+a=input("there is a green and red door which do you choose:")
+if a==("green"):
+        print("you've passed level one:")
+        b = ("you see two alley do you want to go left or right:")
+        if b==("left"):
+            print("you've passed level two:")
+            c = ("You see a dog coming doyou run or do you stay:")
+            if c==("run"):
+                print("you've passed level three:")
+                d = ("You get to a cliff there is water below do you jump or wait:")
+                if d==("jump"):
+                    print("congratulations you have won the game:")
+                else:
+                    print("you have missed the game:")
+            else:
+                 print("you failed at stage three:")
+        else:
+            print("you failed at stage two:")
+else:
+    print("you failed at stage one")  
+                        
+                        
+                         
+                                     
+                
+
+
+            
+
+
+
+
+
+
+
+
+
+
